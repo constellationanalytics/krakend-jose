7240aacfb00dfbf0bcbf13f62d45b549631d3155
Niger Little-Poole <nlittlepoole@gmail.com>
2020-06-09 08:54:32 -0400

    Update jose_example_test.go

--- gin/jose_example_test.go
+++ gin/jose_example_test.go
@@ -10,7 +10,7 @@ import (
 	"strings"
 	"time"
 
-	krakendjose "github.com/devopsfaith/krakend-jose"
+	krakendjose "github.com/constellationanalytics/krakend-jose"
 	"github.com/devopsfaith/krakend/config"
 	"github.com/devopsfaith/krakend/logging"
 	"github.com/devopsfaith/krakend/proxy"
